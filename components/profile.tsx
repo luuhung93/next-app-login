a35fde80f0cac86682da6f88e834af52dce0bee4
L Hung <luuhung93@gmail.com>
2023-06-21 10:31:20 +0700

    note - css

--- components/profile.tsx
+++ components/profile.tsx
@@ -7,7 +7,7 @@ export const Profile = ({ user }: any) => {
             <img className="mx-auto h-48 w-48 rounded-full md:h-56 md:w-56" src={user.image} alt={user.name} />
             <h3 className="mt-6 text-base font-semibold leading-7 tracking-tight text-white">{user.name}</h3>
             <p className="text-sm leading-6 text-gray-400">{user.email}</p>
-            <p className="mt-6 text-xs cursor-pointer" onClick={() => signOut()}>LogOut</p>
+            <p className="mt-6 text-xs cursor-pointer text-white" onClick={() => signOut()}>LogOut</p>
         </div>
     )
 }
